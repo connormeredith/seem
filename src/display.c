6ea08dae80991290628d0435913ebf12f2ce1566
silentham <connor.meredith3@gmail.com>
2014-11-12 22:32:40 +0000

    Removed the need for lineMapping lookup

--- src/display.c
+++ src/display.c
@@ -20,17 +20,6 @@ void initDisplay(u8 memory[]) {
   SDL_Delay(10000);
 }
 
-static int lineMapping[64] = {	
-	0,8,16,24,32,40,48,56,
-	1,9,17,25,33,41,49,57,
-	2,10,18,26,34,42,50,58,
-	3,11,19,27,35,43,51,59,
-	4,12,20,28,36,44,52,60,
-	5,13,21,29,37,45,53,61,
-	6,14,22,30,38,46,54,62,
-	7,15,23,31,39,47,55,63,
-};
-
 void draw(u8 memory[], SDL_Surface* surface) {
 	unsigned int *ptr = (unsigned int*)surface->pixels;
 	u8 actualRow, displayByte, attributeByte, currentPixelBit;
@@ -41,7 +30,7 @@ void draw(u8 memory[], SDL_Surface* surface) {
 		if((vCount % 8) == 0 && vCount != 0) offset++;
 		if((vCount % 64) == 0 && vCount != 0) offset = (offset - 8) + 64;
 
-		actualRow = (lineMapping[(vCount % 8)] + offset);
+		actualRow = (((vCount % 8) * 8) + offset);
 		rowPointer = actualRow * (surface->pitch / sizeof(unsigned int));
 
 		for(int hCount = 0; hCount < 256; hCount++) {
